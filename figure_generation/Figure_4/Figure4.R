06cb1b0d8a9840f74e0c43eb0d00d3d162efd988
GTOP-Network <gtop-contact@smart.org.cn>
2026-02-09 20:32:38 +0800

    Add files via upload

--- figure_generation/Figure_4/Figure4.R
+++ figure_generation/Figure_4/Figure4.R
@@ -3,7 +3,7 @@
 # Figure-4 #
 #==================================#
 
-setwd(".")
+setwd("/media/london_A/mengxin/GTOP_code/fig-4")
 
 library(data.table)
 library(tidyverse)
@@ -13,7 +13,7 @@ library(EnsDb.Hsapiens.v86)
 library(patchwork)
 
 
-## Fig. 4a, Geographic frequencies of eQTL -------------------------------------
+## Fig.4a, Geographic frequencies of eQTL -------------------------------------
 
 if (!py_module_available("matplotlib")) {
   py_install("matplotlib")
@@ -48,7 +48,7 @@ plt.savefig("./Fig4a.pdf", dpi=300, bbox_inches=\'tight\')
 ')
 
 
-## Fig 4b, Allele frequency difference -----------------------------------------
+## Fig.4b, Allele frequency difference -----------------------------------------
 
 freq_plot_data <- fread("./input/Fig4b.txt")
 
@@ -79,7 +79,7 @@ length(freq_plot_data$variant_proxy[freq_plot_data$proxy_delta_group%in%
   length(freq_plot_data$variant_proxy)
 
 
-## Fig 4c, Distribution of fd-QTL variants -------------------------------------
+## Fig.4c, Distribution of fd-QTL variants -------------------------------------
 
 freq_for_type <- fread("./input/Fig4c.txt")
 
@@ -89,11 +89,11 @@ ggplot(data = freq_for_type, aes(x=proxy_delta_group, y=value, fill=variable)) +
   theme(axis.text.x = element_text(color = "black", angle = 30, vjust = 1, hjust = 1),
         axis.text.y = element_text(color = "black"),
         axis.ticks = element_line(color = "black")) +
-  scale_fill_manual(values = c("#0f3d7b", "#7d8cad")) +
+  scale_fill_manual(values = c( "#7d8cad","#0f3d7b")) +
   labs(x="", y="Proportion of fine-mapped variants")
 
 
-## Fig 4d, Distribution of fd-QTL genes ----------------------------------------
+## Fig.4d, Distribution of fd-QTL genes ----------------------------------------
 
 ff_gene_summary <- fread("./input/Fig4d.txt")
 color_vec <- readRDS("./input/tissue_color.RDS")
@@ -109,7 +109,7 @@ ggplot(ff_gene_summary, aes(x = Gene_count, y = new_two_group)) +
   labs(x="", y="Gene number")
 
 
-## Fig 4e, fd-eQTL example -----------------------------------------------------
+## Fig.4e, fd-eQTL example -----------------------------------------------------
 load("./input/Fig4e.RData")
 
 SNP_name <- GWAS_data$rsid[which.min(GWAS_data$p)]
@@ -150,7 +150,7 @@ gene_plot <- gg_genetracks(SNV_eQTL_locus, highlight = "STIM1",
 wrap_plots(list(GWAS_plot, SNV_eQTL_plot, gene_plot), ncol = 1, heights = c(3,3,1))
 
 
-## Fig 4f he-QTL ---------------------------------------------------------------
+## Fig.4f he-QTL ---------------------------------------------------------------
 count_df <- fread("./input/Fig4f.txt")
 color_vec <- readRDS("./input/tissue_color.RDS")
 
@@ -167,15 +167,173 @@ ggplot(count_df, aes(count, reorder(tissue, count), color=tissue)) +
         axis.text.y = element_text(color = "black"))
 
 
-## Fig 4g Z-score of he-QTL between GTOP and GTEx ------------------------------
+## Fig.4g Z-score of he-QTL between GTOP and GTEx ------------------------------
 count_df <- fread("./input/Fig4g.txt")
 
-ggplot(data = plot_df, aes(x = type1, y = abs(value), fill=type1)) + 
+ggplot(data = count_df, aes(x = type1, y = abs(value), fill=type1)) + 
   geom_violin() +
   geom_boxplot(width=0.1, outliers = FALSE) +
   theme_classic() +
-  scale_fill_manual(values = c("GTEx"="#227377", "GTOP"="#8c3426")) +
+  scale_fill_manual(values = c("GTEx"="#e7c798", "GTOP"="#bd5a45")) +
   theme(axis.text = element_text(color = "black"),
         axis.ticks = element_line(color = "black")) +
   labs(x="", y="|Z-score|")
 
+
+# Fig.4h cs size compare----------------------------------------
+library(data.table)
+library(dplyr)
+library(magrittr)
+library(ggplot2)
+library(ggpubr)
+
+dat.gtex <- readRDS("./input/dat.gtex.RDS")
+dat.gtop <- readRDS("./input/dat.gtop.RDS")
+dat.cran <- readRDS("./input/dat.cran.RDS")
+# examine CS number per gene
+df.gtex <- dat.gtex %>% select(locus_id,cs,Tissue) %>% distinct(.keep_all = T)
+df.gtop <- dat.gtop %>% select(locus_id,cs,Tissue) %>% distinct(.keep_all = T)
+df.cran <- dat.cran %>% select(Gene,CS_ID,Tissue) %>% distinct(.keep_all = T)
+
+
+df_count.cran <- df.cran %>% group_by(Gene,Tissue) %>% summarise(cs_count=n())
+df_count.gtex <- df.gtex %>% group_by(locus_id,Tissue) %>% summarise(cs_count=n())
+df_count.gtop <- df.gtop %>% group_by(locus_id,Tissue) %>% summarise(cs_count=n())
+
+df_count.cran$group <- "GTEx+GTOP"
+df_count.gtex$group <- "GTEx"
+df_count.gtop$group <- "GTOP"
+
+names(df_count.gtex) <- c("Gene","Tissue","cs_count","group")
+names(df_count.gtop) <- c("Gene","Tissue","cs_count","group")
+df_count <- rbind(df_count.cran,df_count.gtex,df_count.gtop)
+
+# extract genes with single cs
+df_count.cran$tissue_gene <- paste(df_count.cran$Gene,df_count.cran$Tissue,sep=":")
+df_count.gtex$tissue_gene <- paste(df_count.gtex$Gene,df_count.gtex$Tissue,sep = ":")
+df_count.gtop$tissue_gene <- paste(df_count.gtop$Gene,df_count.gtop$Tissue,sep = ":")
+x.cran <- as.character(df_count.cran$tissue_gene[df_count.cran$cs_count==1])
+x.gtex <- as.character(df_count.gtex$tissue_gene[df_count.gtex$cs_count==1])
+x.gtop <- as.character(df_count.gtop$tissue_gene[df_count.gtop$cs_count==1])
+
+overlap_tissuegenes <- intersect(intersect(x.gtex,x.gtop),x.cran)
+
+df.cran <- dat.cran %>% filter(tissue_gene %in% overlap_tissuegenes) %>% select(Gene,CS_ID,MAX_PIP,CS_LENGTH,Tissue,tissue_gene)
+df.gtex <- dat.gtex %>% filter(tissue_gene %in% overlap_tissuegenes) %>% select(locus_id,cs,pip,cs_size,Tissue,tissue_gene) %>% 
+  group_by(locus_id,cs,Tissue) %>% mutate(max_pip = max(pip)) %>% ungroup() %>% filter(pip==max_pip) %>% 
+  select(locus_id,cs,max_pip,cs_size,Tissue,tissue_gene) %>% distinct(.keep_all = T)
+df.gtop <- dat.gtop %>% filter(tissue_gene %in% overlap_tissuegenes) %>% select(locus_id,cs,pip,cs_size,Tissue,tissue_gene) %>% 
+  group_by(locus_id,cs,Tissue) %>% mutate(max_pip = max(pip)) %>% ungroup() %>% filter(pip==max_pip) %>% 
+  select(locus_id,cs,max_pip,cs_size,Tissue,tissue_gene) %>% distinct(.keep_all = T)
+
+df.cran$group <- "GTEx+GTOP"
+df.gtex$group <- "GTEx"
+df.gtop$group <- "GTOP"
+names(df.cran) <- c("locus_id","cs","max_pip","cs_size","Tissue","tissue_gene","group")
+df_plot1 <- rbind(df.cran,df.gtex,df.gtop)
+df_plot1$group <- factor(df_plot1$group,levels = c("GTOP","GTEx","GTEx+GTOP"))
+# all tissue combined
+p1 <- ggplot(df_plot1,aes(x=group,y=log2(cs_size))) +  geom_violin(aes(fill=group)) + 
+  geom_boxplot(width=.2,fill="white") + theme_pubr() + 
+  scale_fill_manual(breaks =c("GTOP","GTEx","GTEx+GTOP"), values = c("#B65844","#E2C396","#7784A3") );p1 # n=3242 cs
+
+
+x <- df.cran %>% mutate(maxPIP_cran=max_pip,CS_size_cran=cs_size) %>% select(tissue_gene,maxPIP_cran,CS_size_cran)
+y <- df.gtex %>% mutate(maxPIP_gtex=max_pip,CS_size_gtex=cs_size) %>% select(tissue_gene,maxPIP_gtex,CS_size_gtex) 
+z <- df.gtop %>% mutate(maxPIP_gtop=max_pip,CS_size_gtop=cs_size) %>% select(tissue_gene,maxPIP_gtop,CS_size_gtop) 
+xy <- merge(x,y,by="tissue_gene")
+xyz <- merge(xy,z,by="tissue_gene")
+
+test1 <- wilcox.test(xyz$CS_size_cran,xyz$CS_size_gtex,alternative = "less",paired = T) # p<2.2e-16
+test2 <- wilcox.test(xyz$CS_size_cran,xyz$CS_size_gtop,alternative = "less",paired = T) # p<2.2e-16
+
+
+
+# Fig.4i plot prop of PIP>0.8 in single population fine-mapping and cross-ancestry fine-mapping------
+
+overlap_tissuegenes <- intersect(dat.cran$tissue_gene,intersect(dat.gtex$tissue_gene,dat.gtop$tissue_gene))
+df.cran <- dat.cran %>% filter(tissue_gene %in% overlap_tissuegenes)
+df.gtex <- dat.gtex %>% filter(tissue_gene %in% overlap_tissuegenes) %>% group_by(tissue_gene,cs) %>% summarize(max_pip=max(pip))
+df.gtop <- dat.gtop %>% filter(tissue_gene %in% overlap_tissuegenes) %>% group_by(tissue_gene,cs) %>% summarize(max_pip=max(pip))
+
+
+df.cran$PIP_bin <- cut(df.cran$MAX_PIP,breaks = c(0,0.8,1.0),labels = c("0-0.8","0.8-1.0"))
+df.gtex$PIP_bin <- cut(df.gtex$max_pip,breaks = c(0,0.8,1.0),labels = c("0-0.8","0.8-1.0"))
+df.gtop$PIP_bin <- cut(df.gtop$max_pip,breaks = c(0,0.8,1.0),labels = c("0-0.8","0.8-1.0"))
+
+
+
+extract_tissue <- function(x){
+  return(strsplit(x,split=":",fixed=T)[[1]][2])
+}
+
+df.gtex$Tissue <- sapply(df.gtex$tissue_gene,extract_tissue)
+df.gtop$Tissue <- sapply(df.gtop$tissue_gene,extract_tissue)
+
+df.gtex_count <-  as.data.frame.matrix(as.matrix(table(df.gtex$Tissue,df.gtex$PIP_bin)))
+df.gtop_count <-  as.data.frame.matrix(as.matrix(table(df.gtop$Tissue,df.gtop$PIP_bin)))
+df.cran_count <-  as.data.frame.matrix(as.matrix(table(df.cran$Tissue,df.cran$PIP_bin)))
+
+df.gtex_count$group <- "GTEx"
+df.gtop_count$group <- "GTOP"
+df.cran_count$group <- "GTEx+GTOP"
+names(df.gtex_count) <- c("lowPIP","highPIP","group")
+names(df.gtop_count) <- c("lowPIP","highPIP","group")
+names(df.cran_count) <- c("lowPIP","highPIP","group")
+
+df.gtex_count %<>% mutate(PRP1=lowPIP/(lowPIP+highPIP),PRP2=highPIP/(lowPIP+highPIP))
+df.gtop_count %<>% mutate(PRP1=lowPIP/(lowPIP+highPIP),PRP2=highPIP/(lowPIP+highPIP))
+df.cran_count %<>% mutate(PRP1=lowPIP/(lowPIP+highPIP),PRP2=highPIP/(lowPIP+highPIP))
+
+df_plot <- rbind(df.gtex_count[,c(-1,-2)],df.gtop_count[,c(-1,-2)],df.cran_count[,c(-1,-2)])
+
+df_plot.l <- df_plot %>% group_by(group) %>% summarise(meanPRP1=mean(PRP1),meanPRP2=mean(PRP2),sdPRP2=sd(PRP2))
+
+library(reshape2)
+df_plot.l2 <- melt(df_plot.l,id.vars = c("group","sdPRP2"))
+df_plot.l2$group <- factor(df_plot.l2$group,levels = c("GTOP","GTEx","GTEx+GTOP"))
+df_plot.l2$variable <- factor(df_plot.l2$variable,levels = c("meanPRP2","meanPRP1"))
+dat_errorbar <- df_plot.l2 %>% filter(variable=="meanPRP1")
+
+p4 <- ggplot(df_plot.l2) + geom_bar(aes(x=group,y=value,fill=variable),stat="identity") + 
+  geom_errorbar(data=dat_errorbar,aes(x=group,ymin = value-sdPRP2,ymax = value+sdPRP2),width=.4)+ theme_pubr() + 
+  scale_fill_manual(breaks = c("meanPRP2","meanPRP1"),values = c("#913627","#7d8caf"));p4
+
+
+# Fig.4j ABO --------------------------------------------------------------
+
+
+df_fm.gtex <- readRDS("./input/ABO.gtex_finemap.RDS")
+df_fm.gtop <- readRDS("./input/ABO.gtop_finemap.RDS")
+df_fm.susiex <- readRDS("./input/ABO.susiex_finemap.RDS")
+
+
+susiex.cs <- c("rs2129834802","rs8176719")
+df_fm.susiex$PIP <- apply(df_fm.susiex[,c(3,4)],1,max)
+df_fm.gtex$in_cs <- 0
+df_fm.gtex$in_cs[!is.na(df_fm.gtex$cs)] <- 1
+
+df_fm.gtop$in_cs <- 0
+df_fm.gtop$in_cs[!is.na(df_fm.gtop$cs)] <- 1
+
+df_fm.gtex %<>% select(rsid156,chrom,pos,pip,in_cs)
+df_fm.gtop %<>% select(variant_id,chrom,pos1,pip,in_cs)
+
+df_fm.susiex$in_cs <- 0
+df_fm.susiex$in_cs[df_fm.susiex$SNP %in% susiex.cs] <- 1
+
+
+
+df_fm.gtex$in_cs <- factor(df_fm.gtex$in_cs)
+df_fm.gtop$in_cs <- factor(df_fm.gtop$in_cs)
+df_fm.susiex$in_cs <- factor(df_fm.susiex$in_cs)
+p1 <- ggplot(df_fm.gtex,aes(x=pos,y=pip,color=in_cs)) + geom_point(size=2) + theme_pubr() + 
+  scale_color_manual(breaks = c(0,1),values = c("grey","red"))+ ylim(0,1);p1
+p2 <- ggplot(df_fm.gtop,aes(x=pos1,y=pip,color=in_cs)) + geom_point(size=2) + theme_pubr() + 
+  scale_color_manual(breaks = c(0,1),values = c("grey","red")) + ylim(0,1);p2
+p3 <- ggplot(df_fm.susiex,aes(x=BP,y=PIP,color=in_cs)) + geom_point(size=2) + theme_pubr() + 
+  scale_color_manual(breaks = c(0,1),values = c("grey","red")) + ylim(0,1);p3
+
+#pdf(file="SuSiEX.example.ABO.liver.pdf",height = 6.8,width = 4.5)
+cowplot::plot_grid(p1,p2,p3,ncol=1,align = "v")
+#dev.off()
