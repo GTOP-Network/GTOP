a5c277a0e85dc03c32d78b549a1d6e7c8f8cd8a3
GTOP-Network <gtop-contact@smart.org.cn>
2026-02-09 14:11:49 +0800

    Add files via upload

--- figure_generation/Supp_Figures/Supp_Figure_19.R
+++ figure_generation/Supp_Figures/Supp_Figure_19.R
@@ -1,24 +1,52 @@
 #==============================================#
-# LRS WGS QC #
+# Proportion of expression variance captured by the PCs  #
 # Supp-Figure-19#
 #==============================================#
+
 library(data.table)
 library(dplyr)
-library(magrittr)
-library(PCAForQTL)
+library(ggpubr)
 library(cowplot)
 
 setwd("/media/london_A/mengxin/GTOP_code/supp/supp_fig19")
-# Supp.Fig.19 splicing PCA ------------------------------------------------
 
 
-reslist <- readRDS("./input/Figure S19.rds")
+# Supp.Fig.19 Cumulative PVE-------------------------------------------------
 
+
+df <- fread("./input/Figure S19.txt")
 figlist <- list()
+type <- unique(df$qtltype)[c(2, 1, 3)]
 
-for (i in 1:length(reslist)) {
-  K_pc_elbow <- runElbow(prcompResult=reslist[[i]])
-  figlist[[i]] <- makeScreePlot(reslist[[i]] ,labels=c("Elbow"),values=c(K_pc_elbow),titleText=names(reslist)[i])
+for (qtltype_t in type) {
+  plotdf <- df %>% dplyr::filter(qtltype==qtltype_t)
+  
+  plotdf$tissue <- factor(plotdf$tissue, levels = plotdf$tissue)
+  
+  
+  scale_factor <- max(plotdf$num_PCs) / max(plotdf$Cumulative_PVE)
+  
+  figlist[[qtltype_t]] <- ggplot(plotdf, aes(x = tissue)) +
+    geom_bar(aes(y = Cumulative_PVE, fill = "pro_explain"), 
+             stat = "identity") +
+    geom_point(aes(y = num_PCs / scale_factor, color = "PCnumber"),
+               shape = 19) +
+    scale_y_continuous(
+      name = "Proportion of expression variance captured by the PCs",
+      limits = c(0, 0.8),
+      sec.axis = sec_axis(~. * scale_factor, 
+                          name = "Number of PCs in xQTL mapping")
+    ) +
+    scale_fill_manual(values = c("pro_explain" = "#c9c9c9")) +
+    scale_color_manual(values = c("PCnumber" = "#9d3929")) +
+    theme_pubr()+
+    theme(axis.text.x = element_blank(),
+          legend.position = "none")+
+    ggtitle(qtltype_t)
 }
 
-cowplot::plot_grid(plotlist = figlist, ncol = 3)
+cowplot::plot_grid(plotlist = figlist[type], ncol = 1)
+
+
+
+
