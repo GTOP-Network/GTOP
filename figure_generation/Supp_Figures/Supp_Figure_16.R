4748de33b8162450cc9c57dac5f71fbc2fc0605a
GTOP-Network <gtop-contact@smart.org.cn>
2026-02-09 11:48:26 +0800

    Add files via upload

---
+++ figure_generation/Supp_Figures/Supp_Figure_16.R
@@ -0,0 +1,28 @@
+#==============================================#
+# expression PCA #
+# Supp-Figure-16#
+#==============================================#
+
+library(data.table)
+library(dplyr)
+library(magrittr)
+library(PCAForQTL)
+library(cowplot)
+
+setwd("/media/london_A/mengxin/GTOP_code/supp/supp_fig16")
+
+# Supp.Fig.16 expression PCA ----------------------------------------------
+
+
+reslist <- readRDS("./input/Figure S16.rds")
+
+figlist <- list()
+
+for (i in 1:length(reslist)) {
+  K_pc_elbow <- runElbow(prcompResult=reslist[[i]])
+  figlist[[i]] <- makeScreePlot(reslist[[i]] ,labels=c("Elbow"),values=c(K_pc_elbow),titleText=names(reslist)[i])
+}
+
+cowplot::plot_grid(plotlist = figlist, ncol = 3)
+
+
